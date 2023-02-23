216c1176a3ac02cf9ccd13f17584edd7a9c3c9fb
atboxshop <atboxshop@gmail.com>
2023-02-23 03:32:00 -0500

    123

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,62 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-#define LED0 0
-int ledd;
+#define PIN            20
+#define NUMPIXELS      64 //only 64 pixel onboard
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB);
+int delayval = 20;
+int brightness = 2;
+int i;
+int x = 200;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(LED0, OUTPUT);
+  pixels.begin(); // This initializes the NeoPixel library.
 
-  for (ledd = 1; ledd < 14; ledd++) 
-  {
-    pinMode(ledd, OUTPUT);
-    digitalWrite(ledd, HIGH);
-  }
-
-  for (ledd = 15; ledd < 18; ledd++) 
-  {
-    pinMode(ledd, OUTPUT);
-    digitalWrite(ledd, HIGH);
-  }
-
- for (ledd = 33; ledd < 46; ledd++) 
-  {
-    pinMode(ledd, OUTPUT);
-    digitalWrite(ledd, HIGH);
-  }
-
-  for (ledd = 18; ledd < 22; ledd++) 
-  {
-    pinMode(ledd, OUTPUT);
-    digitalWrite(ledd, HIGH);
-  }
-
-  pinMode(26, OUTPUT);
-  digitalWrite(26, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED0, HIGH); //Turn on
-  Serial.println("LED ON");
-  delay (2000); //Wait 1 sec
-  digitalWrite(LED0, LOW); //Turn off
-  Serial.println("LED OFF");
-  delay (2000); //Wait 1 sec
+  if (x==100)
+  {
+    x=255;
+  }
+  for(i=0; i <= 64; i++)
+  {
+    Serial.println("R");
+    pixels.setPixelColor(i, pixels.Color(x,0,0)); //red
+    pixels.setBrightness(brightness);
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval);
+  }
+  //delay(1000); // Delay for a period of time (in milliseconds).
+  for(i=64; i >= 0; i--)
+  {
+    Serial.println("G");
+    pixels.setPixelColor(i, pixels.Color(0,x,0)); //green
+    pixels.setBrightness(brightness);
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval);
+  }
+  //delay(1000); // Delay for a period of time (in milliseconds).
+  for(i=0; i <= 64; i++)
+  {
+    Serial.println("B");
+    pixels.setPixelColor(i, pixels.Color(0,0,x)); //blue
+    pixels.setBrightness(brightness);
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval);
+  }
+  //delay(1000); // Delay for a period of time (in milliseconds).
+  for(i=0; i <= 64; i++)
+  {
+    Serial.println("OFF");
+    pixels.setPixelColor(i, pixels.Color(0,0,0)); //off
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval);
+  }
+  delay(200);
+  x--;
+  Serial.println(x);
 }
